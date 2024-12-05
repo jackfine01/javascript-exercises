23a05434b0c8d26b151232853e74da24031e01d5
jack <jackfine01@gmail.com>
2024-12-05 17:04:48 -0500

    modified:   05_sumAll/sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,17 +4,13 @@ const sumAll = function(start, end) {
     let endCheck = end % 1 === 0;
 
     let sumNum = 0;
-    let first = 0;
-    let last = 0;
+    let first = start;
+    let last = end;
     let arrayAdd = [first, last];
 
-    if(start>end){
-        first === start;
-        last === end;
-    }
-    else{
-        first === end;
-        last === start;
+    if(first>last){
+        first = end;
+        last = start;
     }
 
     // if(start.typeof != Number == true
