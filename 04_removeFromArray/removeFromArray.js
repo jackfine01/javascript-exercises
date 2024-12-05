a3251d06806f6c5be91921053fd6e312fb02baf8
jack <jackfine01@gmail.com>
2024-12-04 22:38:30 -0500

    built skeleton for 4.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(parameterArray, ...theArgs) {
+
+let filtered = parameterArray;
+
+        for(let i = 0;i<theArgs.length;i++){
+
+            for(let j = 0;j<parameterArray.length;j++){
+
+                if(theArgs[i]==parameterArray[j]){
+
+                    filtered = parameterArray.filter(theArgs[i]); 
+
+                };      
+            };
+        };
+
+return filtered;
+
 };
 
 // Do not edit below this line
