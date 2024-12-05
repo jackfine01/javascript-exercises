937db532d49e6cb3733fb3e8c530387700928f55
jack <jackfine01@gmail.com>
2024-12-04 21:48:23 -0500

    making it so that paramter is actually changed by slice.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,8 +2,8 @@ const reverseString = function(str) {
     let reversedString = "";
     for( let i=str.length ; i > 0 ; i-- ){
         reversedString += str.charAt(str.length - 1);
+        str = str.slice(0,-1);
     };
-    str.slice(0,-1);
     return reversedString;
 };
 
