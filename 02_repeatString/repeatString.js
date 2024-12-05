8430bb8ab9440bfdfcf7fbb7f54d197d2f8ac619
jack <jackfine01@gmail.com>
2024-12-04 21:28:34 -0500

    changing location of return of finalstring, and intialization location.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,9 +2,8 @@ let string = '';
 let num = 1;
 
 const repeatString = function(string, num) {
-
+    let finalstring = '';
     for(let i = 0 ; i<num ; i++ ){
-        let finalstring = '';
         finalstring += string;
     };
     return finalstring;
