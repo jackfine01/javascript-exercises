dd54ad18c96774b526d59e7dd81081476f9104e2
jack <jackfine01@gmail.com>
2024-12-04 21:20:21 -0500

    adding a final string variable into skeleton and for loop.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,12 @@
 let string = '';
 let num = 1;
-
+let finalstring = '';
 const repeatString = function(string, num) {
 
     for(let i = 0 ; i<num ; i++ ){
-
+        finalstring += string;
     }
-
+    return finalstring;
 };
 
 // Do not edit below this line
