eb62e7cd82a0b96f01968138dab3b0fc98a6aa73
jack <jackfine01@gmail.com>
2024-12-05 18:23:53 -0500

    adding new for loops.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    
+    let firstNum = start;
+    let lastNum = last;
+
+    if(start>end){
+        firstNum = end;
+        lastNum = start;
+    };
+
+    let sumArray = [firstNum,lastNum];
+    for(
+        let i = firstNum+1; 
+        i != lastNum; 
+        i++
+    ){
+        sumArray.push(i);
+    };
+
+    let finalSum = 0;
+    for(
+        let j = 0;
+        j < sumArray.length;
+        j++
+    ){
+        finalSum += sumArray[j];
+    };
 
 };
 
