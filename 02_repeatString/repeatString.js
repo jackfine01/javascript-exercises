729a2b475a6dee8c2269d34a7198c9049bdf21b7
jack <jackfine01@gmail.com>
2024-12-04 21:27:20 -0500

    changing intialzation location of final string to make it local to repeatString.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,13 +1,13 @@
 let string = '';
 let num = 1;
-let finalstring = '';
+
 const repeatString = function(string, num) {
 
     for(let i = 0 ; i<num ; i++ ){
+        let finalstring = '';
         finalstring += string;
         return finalstring;
     };
 };
-finalstring = '';
 // Do not edit below this line
 module.exports = repeatString;
