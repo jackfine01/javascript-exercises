938eb129de99fc715e96913d0a05d704f14e0e95
jack <jackfine01@gmail.com>
2024-12-05 13:27:29 -0500

    adding a splcie function to if statement.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,13 +3,9 @@ const removeFromArray = function(parameterArray, ...theArgs) {
 let filtered = parameterArray;
 
         for(let i = 0;i<theArgs.length;i++){
-
             for(let j = 0;j<parameterArray.length;j++){
-
                 if(theArgs[i]==parameterArray[j]){
-
-                    filtered = parameterArray.filter(theArgs[j]); 
-
+                    parameterArray.splice(j,1);
                 };      
             };
         };
