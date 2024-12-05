4b171d0e1f650bde21780a220803a005e3f804b0
jack <jackfine01@gmail.com>
2024-12-04 21:23:47 -0500

    changing so that finalstring resets after each loop.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -5,8 +5,10 @@ const repeatString = function(string, num) {
 
     for(let i = 0 ; i<num ; i++ ){
         finalstring += string;
-    }
-    return finalstring;
+        return finalstring;
+        finalstring = '';
+    };
+    
 };
 
 // Do not edit below this line
