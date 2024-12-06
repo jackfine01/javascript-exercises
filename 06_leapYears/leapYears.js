621b01a68efe521b5fba2ea5aedc48315e676678
jack <jackfine01@gmail.com>
2024-12-05 19:04:53 -0500

    starting leap years.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    let isCent = false;
+    if(year % 100 === 0){
+        isCent = true;
+    }
+
+    let isFourCent = false;
+    if(year % 400 === 0){
+        isFourCent = true;
+    };
+
+    let isFour = false;
+    if(year % 4 === 0){
+        isFour = true;
+    };
+    let final = false;
+    if(isFour === true && isCent === false && isFourCent === true) {
+        final = true;
+    }
+    return final;
 };
 
 // Do not edit below this line
